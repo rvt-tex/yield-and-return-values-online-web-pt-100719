c5d416f0efece70b7fa4b0eefedef4ed59fbbf4b
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 03:02:06 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -21,3 +21,14 @@ def greetings(array)
 end
 
 greetings(["john", "Junior", "jim", "mark"]) { |name| puts "Hi, #{name}" }
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
