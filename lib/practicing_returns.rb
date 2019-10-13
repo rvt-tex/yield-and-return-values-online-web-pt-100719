285ac112d456c7a39dda7056b7a6b1176f2d31dd
Ruel Tingling <rvtingling@gmail.com>
2019-10-13 03:02:32 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -7,6 +7,28 @@ def hello(array)
     i += 1
   end
 end
-
+binding.pry
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+def greetings(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+end
+
+greetings(["john", "Junior", "jim", "mark"]) { |name| puts "Hi, #{name}" }
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
